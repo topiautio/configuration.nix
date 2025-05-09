8241bcfb23ae704b8c4dc40b1d3f9777f9d16d52
topiautio <64556883+topiautio@users.noreply.github.com>
2025-05-09 21:16:18 +0300

    update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url        = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";  # use the same pkgs set
@@ -13,15 +13,16 @@
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-      inherit system pkgs;
+      system  = system;
       modules = [
-        ./configuration.nix        # your system module
+        ./nixos/configuration.nix        # your system module
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs    = true;  # reuse system pkgs
           home-manager.users.topi = import ./home.nix;
         }
       ];
+      specialArgs = { inherit pkgs; };
     };
   };
 }
